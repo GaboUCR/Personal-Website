934b3fb9d199c3f4058fadf338251213627bbd00
gabrielgamboa <gabrielgamboav19@gmail.com>
2021-08-06 18:47:07 -0600

    Able to pass html through handler

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+import markdown
 
 app = Flask(__name__)
 app.config.from_mapping(SECRET_KEY="pepino")
@@ -6,3 +8,14 @@ app.config.from_mapping(SECRET_KEY="pepino")
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/projects")
+def projects():
+    dir = os.getcwd()+"\\server\\templates\\projects"
+
+    projectsName = [project for project in os.listdir(dir)]
+
+    projects = [markdown.markdown(open(dir+"\\"+projectName).read()) \
+                for projectName in projectsName]
+
+    return render_template('projects.html', projects=projects)
